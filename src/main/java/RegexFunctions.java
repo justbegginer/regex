e8b960f0b6be09a21213f2f0e8381df2de09a744
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-16 13:40:30 +0300

    refactor RegexFunctions and delete repeatable parts in list functions

--- src/main/java/RegexFunctions.java
+++ src/main/java/RegexFunctions.java
@@ -11,29 +11,28 @@ public class RegexFunctions {
         return correctionChecker(str, "^(8|(\\+7))[0-9]{10}$");
     }
     public static List<String> correctPhoneNumberList(List<String> array){
-        return array.stream()
-                .filter(str -> isPhoneNumberCorrect(str))
-                .collect(Collectors.toList());
+        return getList(array, str -> isPhoneNumberCorrect(str));
     }
     public static boolean isEmailCorrect(String str){
         return correctionChecker(str, "^[a-z](\\.?[a-z]+)+@[a-z](\\.?[a-z]+)+$");
     }
     public static List correctEmailList(List<String> array){
-        return array.stream()
-                .filter(str -> isEmailCorrect(str))
-                .collect(Collectors.toList());
+        return getList(array, str -> isEmailCorrect(str));
     }
     public static boolean isMacCorrect(String str){
         return correctionChecker(str, "^([a-fA-F0-9]{2}(\\.|-)){5}[a-fA-F0-9]{2}$");
     }
     public static List<String> correctMacList(List<String> array){
-        return array.stream()
-                .filter(str -> isMacCorrect(str))
-                .collect(Collectors.toList());
+        return getList(array, str -> isMacCorrect(str));
     }
     private static boolean correctionChecker(String str, String patternString){
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(str);
         return matcher.find();
+    }
+    private static List<String> getList(List<String> array, Predicate<String> predicate){
+        return array.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }
