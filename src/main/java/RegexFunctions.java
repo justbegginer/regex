5c81638cf08274606dd13983c94fe319646db3b5
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-16 13:37:30 +0300

    refactor RegexFunctions and delete repeatable parts in correction checker functions

--- src/main/java/RegexFunctions.java
+++ src/main/java/RegexFunctions.java
@@ -1,14 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RegexFunctions {
     public static boolean isPhoneNumberCorrect(String str){
-        Pattern pattern = Pattern.compile("^(8|(\\+7))[0-9]{10}$");
-        Matcher matcher = pattern.matcher(str);
-        return matcher.find();
+        return correctionChecker(str, "^(8|(\\+7))[0-9]{10}$");
     }
     public static List<String> correctPhoneNumberList(List<String> array){
         return array.stream()
@@ -16,9 +16,7 @@ public class RegexFunctions {
                 .collect(Collectors.toList());
     }
     public static boolean isEmailCorrect(String str){
-        Pattern pattern = Pattern.compile("^[a-z](\\.?[a-z]+)+@[a-z](\\.?[a-z]+)+$");
-        Matcher matcher = pattern.matcher(str);
-        return matcher.find();
+        return correctionChecker(str, "^[a-z](\\.?[a-z]+)+@[a-z](\\.?[a-z]+)+$");
     }
     public static List correctEmailList(List<String> array){
         return array.stream()
@@ -26,13 +24,16 @@ public class RegexFunctions {
                 .collect(Collectors.toList());
     }
     public static boolean isMacCorrect(String str){
-        Pattern pattern = Pattern.compile("^([a-fA-F0-9]{2}(\\.|-)){5}[a-fA-F0-9]{2}$");
-        Matcher matcher = pattern.matcher(str);
-        return matcher.find();
+        return correctionChecker(str, "^([a-fA-F0-9]{2}(\\.|-)){5}[a-fA-F0-9]{2}$");
     }
-    public static List correctMacList(List<String> array){
+    public static List<String> correctMacList(List<String> array){
         return array.stream()
                 .filter(str -> isMacCorrect(str))
                 .collect(Collectors.toList());
+    }
+    private static boolean correctionChecker(String str, String patternString){
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
     }
 }
