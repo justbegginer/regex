7d37ffeb8e4a4efd481f11e257b00b041c7f31ad
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-18 23:10:42 +0300

    refactoring

--- src/main/java/RegexFunctions.java
+++ src/main/java/RegexFunctions.java
@@ -7,38 +7,53 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class RegexFunctions {
-    public static boolean isPhoneNumberCorrect(String str){
+    public static boolean isPhoneNumberCorrect(String str) {
         return correctionChecker(str, "^(8|(\\+7))[0-9]{10}$");
     }
-    public static List<String> correctPhoneNumberList(List<String> array){
-        return getList(array, str -> isPhoneNumberCorrect(str));
+
+    public static List<String> correctPhoneNumberList(List<String> array) {
+        return getList(array, RegexFunctions::isPhoneNumberCorrect);
     }
-    public static boolean isEmailCorrect(String str){
+
+    public static boolean isEmailCorrect(String str) {
         return correctionChecker(str, "^[a-z](\\.?[a-z]+)+@[a-z](\\.?[a-z]+)+$");
     }
-    public static List correctEmailList(List<String> array){
-        return getList(array, str -> isEmailCorrect(str));
+
+    public static List correctEmailList(List<String> array) {
+        return getList(array, RegexFunctions::isEmailCorrect);
     }
-    public static boolean isMacCorrect(String str){
+
+    public static boolean isMacCorrect(String str) {
         return correctionChecker(str, "^([a-fA-F0-9]{2}(\\.|-)){5}[a-fA-F0-9]{2}$");
     }
-    public static List<String> correctMacList(List<String> array){
-        return getList(array, str -> isMacCorrect(str));
+
+    public static List<String> correctMacList(List<String> array) {
+        return getList(array, RegexFunctions::isMacCorrect);
     }
-    public static boolean isDateCorrect(String str){
+
+    public static boolean isDateCorrect(String str) {
         return correctionChecker(str, "^([1]?[0-9]{0,3}|201[0-9]|202[012]|200[0-9])\\/(1[012]|[0-9])\\/(30|[12]?[0-9])$");
     }
-    public static List<String> correctDataList(List<String> array){ return getList(array, str -> isDateCorrect(str)); }
-    public static boolean isIpCorrect(String str){
+
+    public static List<String> correctDataList(List<String> array) {
+        return getList(array, RegexFunctions::isDateCorrect);
+    }
+
+    public static boolean isIpCorrect(String str) {
         return correctionChecker(str, "^((1[0-9]{2}|2[0-4][0-9]|[0-9]{1,2}|25[0-6])\\.){3}(1[0-9]{2}|2[0-4][0-9]|[0-9]{1,2}|25[0-6])$");
     }
-    public static List<String> correctIpList(List<String> array){ return getList(array, str -> isIpCorrect(str));}
-    private static boolean correctionChecker(String str, String patternString){
+
+    public static List<String> correctIpList(List<String> array) {
+        return getList(array, RegexFunctions::isIpCorrect);
+    }
+
+    private static boolean correctionChecker(String str, String patternString) {
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(str);
         return matcher.find();
     }
-    private static List<String> getList(List<String> array, Predicate<String> predicate){
+
+    private static List<String> getList(List<String> array, Predicate<String> predicate) {
         return array.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
