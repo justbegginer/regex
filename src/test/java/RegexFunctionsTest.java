7821307e8dfbba0d5cfcc6c587d947be1c32c30c
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-27 20:42:36 +0300

    remake test functions dependent to the flag

--- src/test/java/RegexFunctionsTest.java
+++ src/test/java/RegexFunctionsTest.java
@@ -16,6 +16,11 @@ class RegexFunctionsTest {
         assertFalse(RegexFunctions.isPhoneNumberCorrect("8931972129"));
         assertFalse(RegexFunctions.isPhoneNumberCorrect("+793197212921"));
         assertFalse(RegexFunctions.isPhoneNumberCorrect("+99319721292"));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isPhoneNumberCorrect("Nikita - +79319921293"));
+        assertTrue(RegexFunctions.isPhoneNumberCorrect("Artem - 89319921293(his number)"));
+        assertFalse(RegexFunctions.isPhoneNumberCorrect("Nikita - +9319921293"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -40,6 +45,10 @@ class RegexFunctionsTest {
         assertFalse(RegexFunctions.isEmailCorrect("dafs.daf.@afaaf.dad.fa.ad"));
         assertFalse(RegexFunctions.isEmailCorrect(".dafsaf@dasf.com"));
         assertFalse(RegexFunctions.isEmailCorrect("a@daf.cae"));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isEmailCorrect("My email - aa@daf.cae"));
+        assertFalse(RegexFunctions.isEmailCorrect("Wrong email - dafs.daf.@afaaf.dad.fa.ad"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -62,6 +71,10 @@ class RegexFunctionsTest {
         assertTrue(RegexFunctions.isMacCorrect("ad-ad-da-da-da-da"));
         assertFalse(RegexFunctions.isMacCorrect("dada.da.34.65.65.54"));
         assertFalse(RegexFunctions.isMacCorrect("fs.43.54.65.76.45"));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isMacCorrect("MAC of my friend - 12.af.32.43.43.a3"));
+        assertFalse(RegexFunctions.isMacCorrect("Incorrect MAC - z.da.34.65.65.54"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -93,6 +106,10 @@ class RegexFunctionsTest {
         assertFalse(RegexFunctions.isIpCorrect("2.2.4."));
         assertFalse(RegexFunctions.isIpCorrect("257.2.4.2"));
         assertFalse(RegexFunctions.isIpCorrect("2.300.4.12"));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isIpCorrect("IP230.43.132.32Ip"));
+        assertFalse(RegexFunctions.isIpCorrect("Error2.2.4.Error"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -103,6 +120,10 @@ class RegexFunctionsTest {
         assertFalse(RegexFunctions.isLinkCorrect("hhdj://ksdd.re"));
         assertFalse(RegexFunctions.isLinkCorrect("http://dsgsvd"));
         assertFalse(RegexFunctions.isLinkCorrect("http://dsgsvd."));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isLinkCorrect("I am in https://vk.com"));
+        assertFalse(RegexFunctions.isLinkCorrect("Error - hhdj://ksdd.re STATUS-404"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -116,14 +137,12 @@ class RegexFunctionsTest {
             }
 
         }
-        for (int i = 0; i < 24; i++) {
-            for (int j = 0; j < 60; j++) {
-                assertTrue(RegexFunctions.isTimeCorrect(String.format("%d:%d", i, j)));
-            }
-
-        }
         assertFalse(RegexFunctions.isTimeCorrect("23:60"));
         assertFalse(RegexFunctions.isTimeCorrect("30:30"));
+        RegexFunctions.setPatternAllString(false);
+        assertTrue(RegexFunctions.isTimeCorrect( "Now - 23:23 o'clock"));
+        assertFalse(RegexFunctions.isTimeCorrect("Time can't be 23:60, in hour 60 minute, not 61 :)"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
@@ -134,6 +153,10 @@ class RegexFunctionsTest {
         assertFalse(RegexFunctions.isCarNumberCorrect("r002ree"));
         assertFalse(RegexFunctions.isCarNumberCorrect("r02re"));
         assertFalse(RegexFunctions.isCarNumberCorrect("r0002re"));
+        RegexFunctions.setPatternAllString(false);
+        assertFalse(RegexFunctions.isCarNumberCorrect("You give me wrong number - r02re"));
+        assertTrue(RegexFunctions.isCarNumberCorrect("The car number of intruder a103bv"));
+        RegexFunctions.setPatternAllString(true);
     }
 
     @Test
